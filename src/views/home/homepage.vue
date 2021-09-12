8289b4154a4769a4970ec742df018fb2426a95fd
lsh <shenlirenneversaynever@gmail.com>
2021-09-13 00:04:03 +0800

    HomePage 修复

--- src/views/home/homepage.vue
+++ src/views/home/homepage.vue
@@ -12,8 +12,9 @@
                     is-nav>
           <b-navbar-nav>
             <b-nav-item href="#">Link</b-nav-item>
+
             <b-nav-item href="#"
-                        disabled>Disabled</b-nav-item>
+                        disabled>Disabledp</b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
